825da1e3a4e65bf9ce2e50b88f8f6f2e78b4bb3f
nakada toyohisa <nakada.toyohisa@gmail.com>
2026-07-02 10:26:25 +0900

    0709

---
+++ problems/iu_2026/d20260709_1/02.js
@@ -0,0 +1,103 @@
+new function(){
+	var scriptName = plib.getScriptName();
+	var errorMessages = ["違います。"];
+	var errorNo = 0;
+
+	var self_n = problems.problems.push([
+		function(){
+			plib.log.add(scriptName+":startProblem");
+			window.exec({module:"main",command:"updateStatus",params:{scriptName:scriptName}});
+
+			["input","code","output","watch","hint"].forEach(function(elem){
+				window.exec({module:elem,command:"clear"});
+				window.exec({module:elem,command:"clearEvent"});
+				window.exec({module:elem,command:"clearInput"});
+			});
+			window.exec({module:"main",command:"closeReq"});
+			["input","code","output"].forEach(function(elem){
+				window.exec({module:elem,command:"autoSave",params:{pnumber:scriptName}});
+			});
+
+			plib.setExpectedOutputs(["出力 b","出力 a","出力 same"]);
+			window.exec({module:"input",command:"setInitial",params:{
+				pnumber:scriptName,
+				message:plib.getExpectedOutputs(),
+				value:[
+					{name:"a",initValue:["1","4","5"]},
+					{name:"b",initValue:["2","3","5"]},
+				],
+			}});
+			window.exec({module:"watch",command:"addValue",params:{name:"a"}});
+			window.exec({module:"watch",command:"addValue",params:{name:"b"}});
+			window.exec({module:"output",command:"setInitial",params:{pnumber:scriptName}});
+			window.exec({module:"scripts",command:"setScriptName",params:scriptName});
+			window.exec({module:"code",command:"setInitialText",params:{
+				setEditable:[[{line:3,ch:0},{line:3,ch:4}],[{line:5,ch:1},{line:5,ch:5}],[{line:7,ch:1},{line:7,ch:5}]],
+				text:"\
+\/\/ aとbの値が同じ場合はsame、aの方が大きい場合はa、\n\
+\/\/ bの場合はbと表示するプログラムを作成せよ。\n\
+ \n\
+    {\n\
+	print(\"a\");\n\
+}    {\n\
+	print(\"b\");\n\
+}    {\n\
+	print(\"same\");\n\
+}\n\
+\n\
+"}});
+
+			window.exec({module:"input",command:"enable"});
+			window.exec({module:"input",command:"setReadOnly"});
+			window.exec({module:"code",command:"enable"});
+//			window.exec({module:"code",command:"setReadOnly"});
+
+			HINT.setScriptName(scriptName);
+			HINT.hint("equal2");
+			HINT.hint("greater");
+			HINT.hint("if_elseif_else");
+
+			plib.startOutputCheck();
+			problems.next();
+		},
+		function(){
+			var w = $("#code")[0].contentWindow;
+			w.$(".CodeMirror").instruct({
+				string:"この指示に従ったプログラムを作成してください。",
+				closeButton:true,
+				closedHandler:function(){},
+			});
+		},
+/*
+		function(){
+			var w = $("#input")[0].contentWindow;
+			w.$("#datasetPanel").instruct({
+				string:"また、ここに複数の入力データセットがあります。クリックしてデータセットを変更し、すべてのデータで正しく出力されることを確認して下さい。",
+				closeButton:true,
+				closedHandler:function(){
+				},
+			});
+		},
+*/
+		function(){
+			plib.log.add(scriptName+":finished_problem");
+			window.exec({
+				module:"main",
+				command:"writeLog",
+				params:{message:plib.log.text()}});
+			plib.log.init();
+			$("body").instruct({
+				string:"おめでとうございます !!",
+				align:'center',
+				arrow:false,
+				font_size:'72px',
+				offsetX:$("body").width()/2,
+				offsetY:$("body").height()/2+200,
+				targetEventToClose:null,
+				closeButton:true,
+				closedHandler:function(){problems.next();},
+			});
+		},
+	]);
+}
+
